5b18c4f848beb1e68045844fcae8c97cf2cb1d4d
Daveadj <Dajerese@vatebra.com>
2024-12-02 19:10:18 +0100

    update

--- Services/Ordering/Ordering.Api/Program.cs
+++ Services/Ordering/Ordering.Api/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.Api;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
@@ -9,5 +10,12 @@ builder.Services
     .AddApiServices();
 
 var app = builder.Build();
+
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
