8c51cf200b52956e2a23f36866b6113b6a1fa09a
Daveadj <Dajerese@vatebra.com>
2025-01-07 18:13:30 +0100

    update

--- ApiGateway/YarpApiGateway/Program.cs
+++ ApiGateway/YarpApiGateway/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-var app = builder.Build();
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
+var app = builder.Build();
+app.UseRateLimiter();
 app.MapReverseProxy();
 app.Run();
